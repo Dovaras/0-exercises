cc1f451b27bd0983e0372803ef53b914cab730da
Arūnas Dovainis <arunas.dovainis@gmail.com>
2021-07-11 10:28:29 +0300

    Antras uzdavinys

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const allIngredients = document.querySelectorAll('li');
+
+//allIngredients.forEach(element => console.log(element));
+
+allIngredients.toUpperCase();
+
+//allIngredients.forEach(element => console.log(element));
+
+for (const i = 0; i < allIngredients.length; i++){
+    console.log(i);
+}
